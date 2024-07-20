ebdcee393fcd69bff474ef97f8dde680c1dff720
vietkey753 <118911275+vietkey753@users.noreply.github.com>
2024-07-20 17:50:14 +0700

    Add files via upload

---
+++ mailer/send-mail.php
@@ -0,0 +1,38 @@
+
+
+    <?php
+
+    require("mailer/src/PHPMailer.php");
+    require("mailer/src/SMTP.php");
+    require("mailer/src/Exception.php");
+
+
+    function sendEmail($recipientEmail, $title, $content)
+    {
+        $mail = new PHPMailer\PHPMailer\PHPMailer();
+        $mail->IsSMTP();
+        $mail->CharSet = 'UTF-8';
+        $mail->SMTPDebug = 0;
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = 'ssl';
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 465;
+        $mail->IsHTML(true);
+        $mail->Username = 'nguyenleanhkhoa25866@gmail.com';
+        $mail->Password = 'cprzeejxblkesolz';
+        // $mail->SetFrom('Admin');
+        // Thay đổi tên người gửi và địa chỉ email người gửi
+        $mail->SetFrom('nguyenleanhkhoa25866@gmail.com', 'FAHASA shop');
+        $mail->Subject = $title;
+        $mail->Body = $content;
+        $mail->AddAddress($recipientEmail);
+
+        if (!$mail->Send()) {
+            return "Mailer Error: " . $mail->ErrorInfo;
+        } else {
+            return "Gửi thành công";
+        }
+    }
+
+
+    ?>
